ae98faf2622810c7f4cf8f5bdd5769b1893d0e2a
Nicolas Alvarez <nicolas.alvarez@fdvsolutions.com>
2018-04-04 19:10:06 -0300

    First Commit

--- ARKit3DDemo/ViewController.swift
+++ ARKit3DDemo/ViewController.swift
@@ -15,7 +15,54 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addPaperPlane()
+        addCar(x:0, y:-0.5)
+        addTapGestureToSceneView()
+        configureLighting()
+    }
+    
+    func configureLighting() {
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.automaticallyUpdatesLighting = true
+    }
+    
+    func addPaperPlane(x: Float = 0, y: Float = 0, z: Float = -0.5) {
+        guard let paperPlaneScene = SCNScene(named: "paperPlane.scn"), let paperPlaneNode = paperPlaneScene.rootNode.childNode(withName: "paperPlane", recursively: true) else { return }
+        paperPlaneNode.position = SCNVector3(x, y, z)
+        sceneView.scene.rootNode.addChildNode(paperPlaneNode)
+    }
+
+    func addCar(x: Float = 0, y: Float = 0, z: Float = -0.5) {
+        guard let carScene = SCNScene(named: "car.dae") else { return }
+        let carNode = SCNNode()
+        let carSceneChildNodes = carScene.rootNode.childNodes
         
+        for childNode in carSceneChildNodes {
+            carNode.addChildNode(childNode)
+        }
+        
+        carNode.position = SCNVector3(x, y, z)
+        carNode.scale = SCNVector3(0.5, 0.5, 0.5)
+        sceneView.scene.rootNode.addChildNode(carNode)
+    }
+    
+    @objc func didTap(withGestureRecognizer recognizer: UIGestureRecognizer) {
+        let tapLocation = recognizer.location(in: sceneView)
+        let hitTestResults = sceneView.hitTest(tapLocation)
+        guard let node = hitTestResults.first?.node else {
+            let hitTestResultsWithFeaturePoints = sceneView.hitTest(tapLocation, types: .featurePoint)
+            if let hitTestResultWithFeaturePoints = hitTestResultsWithFeaturePoints.first {
+                let translation = hitTestResultWithFeaturePoints.worldTransform.columns
+                addPaperPlane(x: translation.3.x, y: translation.3.y, z: translation.3.z)
+            }
+            return
+        }
+        node.removeFromParentNode()
+    }
+    
+    func addTapGestureToSceneView() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTap(withGestureRecognizer:)))
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
     }
     
     override func viewWillAppear(_ animated: Bool) {
